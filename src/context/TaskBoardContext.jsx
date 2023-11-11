e5dd203ca799b10bcd148ee6d5eda652c80b5a10
frckbrice <maebrie2017@gmail.com>
2023-11-11 13:31:48 +0100

    adding new functionalities on dashbord

--- src/context/TaskBoardContext.jsx
+++ src/context/TaskBoardContext.jsx
@@ -13,8 +13,11 @@ const ContextProvider = ({ children }) => {
   const [isRefreshSuccess, setIsRefreshSuccess] = useState(true);
   const [isRefreshError, setIsRefreshError] = useState(false);
   const [memberofProject, setMembersofProject] = useState([]);
-  const [profilePict, setProfilePict] = useState('');
+  const [profilePict, setProfilePict] = useState("");
   const [isLoad, setIsLoad] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [openAddList, setOpenAddList] = useState(false);
+  const [projects, setProjects] = useState(false);
 
   console.log("project data", projectData);
 
@@ -25,7 +28,8 @@ const ContextProvider = ({ children }) => {
     setProjectData,
     email,
     setEmail,
-   isLoad, setIsLoad,
+    isLoad,
+    setIsLoad,
     isRefreshSuccess,
     setIsRefreshSuccess,
     isRefreshError,
@@ -36,8 +40,14 @@ const ContextProvider = ({ children }) => {
     setProjectStatus,
     memberofProject,
     setMembersofProject,
-    profilePict,
     setProfilePict,
+    profilePict,
+    progress,
+    setProgress,
+    openAddList,
+    setOpenAddList,
+    projects,
+    setProjects,
   };
 
   return <TmsContext.Provider value={values}>{children} </TmsContext.Provider>;
